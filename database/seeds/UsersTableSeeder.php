010f41fdcfc57c22d92c8e309dd3f1632385fb1e
yamakawa <yamakawa@mountain-gorilla.co.jp>
2020-11-06 08:32:29 +0900

    update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $model = new User;$model->name = 'admin';$model->password = bcrypt('9998');$model->is_admin = true;$model->is_leader = true;$model->save();
         $model = new User;$model->name = 'user01';$model->password = bcrypt('1234');$model->is_admin = true;$model->is_leader = false;$model->save();
         $model = new User;$model->name = 'user02';$model->password = bcrypt('1234');$model->is_admin = false;$model->is_leader = true;$model->save();
         $model = new User;$model->name = 'user03';$model->password = bcrypt('1234');$model->is_admin = false;$model->is_leader = false;$model->save();
